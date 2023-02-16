105886d9354b1be3e8d1b4fa9e8a3d49c6650903
ashimachanpuriya <124254677+ashimachanpuriya@users.noreply.github.com>
2023-02-16 15:10:44 +0530

    Update V1.1.4__insert_table.sql

--- dbscripts/V1.1.4__insert_table.sql
+++ dbscripts/V1.1.4__insert_table.sql
@@ -1,7 +1,7 @@
 USE SCHEMA CICD.PUBLIC;
 
 INSERT INTO emp(PersonID , LastName , FirstName, Address , City)
-VALUES (2, 'Erichsennn', 'wxy', 'abc2','Norway');
+VALUES (4, 'Erichsennn', 'wxy', 'abc2','Norway');
 
 INSERT INTO emp(PersonID , LastName , FirstName, Address , City)
 VALUES (3, 'Ashima', 'Chanpuriya', 'abc','xyz');
