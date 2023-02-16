b052d4889146e91916bedbb618237348f5d0ea83
ashimachanpuriya <chanpuriyaashima@gmail.com>
2023-02-16 13:51:34 +0530

    inserting

---
+++ dbscripts/V1.1.4__insert_table.sql
@@ -0,0 +1,10 @@
+USE SCHEMA CICD.PUBLIC;
+
+INSERT INTO emp(PersonID , LastName , FirstName, Address , City)
+VALUES (2, 'Erichsen', 'wxy', 'abc2','Norway');
+
+INSERT INTO emp(PersonID , LastName , FirstName, Address , City)
+VALUES (3, 'Ashima', 'Chanpuriya', 'abc','xyz');
+
+INSERT INTO Persons (PersonID , LastName , FirstName, Address , City)
+VALUES (1, 'Ashima', 'Chanpuriya', 'abc','xyz');
