0adf9a3f8f1795dbe911d9e3103d98b64265a759
ashimachanpuriya <chanpuriyaashima@gmail.com>
2023-02-16 12:57:24 +0530

    adding dbscripts

---
+++ dbscripts/V1.1.4__insert_table.sql
@@ -0,0 +1,7 @@
+USE SCHEMA DEMOT.PUBLIC;
+
+INSERT INTO emp(PersonID , LastName , FirstName, Address , City)
+VALUES (2, 'Erichsen', 'wxy', 'abc2','Norway');
+
+INSERT INTO emp(PersonID , LastName , FirstName, Address , City)
+VALUES (2, 'Ashima', 'Chanpuriya', 'abc','xyz');
